086a01c223e28136e1b9020b0b98df45ddd78c21
camilaramirez10 <camilaram2009@hotmail.com>
2023-11-01 23:03:43 -0500

    Proyecto inicial

---
+++ entregaUno/src/App.jsx
@@ -0,0 +1,18 @@
+import Card from './Components/Card'
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div>
+        <Card style = {{color: "orange"}}/>
+      </div>
+      
+    </>
+  )
+}
+
+export default App
